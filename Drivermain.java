5aa1d2bd6798a05d972cc39c02e8d72042db05e7
abhinavsingh026 <abhinavkrsigh0258@gmail.com>
2025-05-05 20:35:15 +0530

    Ye Leh

---
+++ Drivermain.java
@@ -0,0 +1,14 @@
+class Test{
+    void add(int a,char b){
+        System.out.println(a+b);
+    }
+    void add(int a,double b){
+        System.out.println(a+b);
+    }
+}
+class Drivermain{
+    public static void main(String[]args){
+        Test obj = new Test();
+        obj.add(11,11);
+    }
+}
