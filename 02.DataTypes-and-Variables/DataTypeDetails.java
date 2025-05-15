c447c9fbc41f1f84f3eee25a9d75ae3ac9b058b6
abhinavsingh026 <abhinavkrsingh0258@gmail.com>
2025-05-15 22:00:01 +0530

    Changing decimal into other no. systems

---
+++ 02.DataTypes-and-Variables/DataTypeDetails.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+class DataTypeDetails {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Decimal No. : ");
+        int n = sc.nextInt();
+        System.out.println("Given No. in Binary is: "+Integer.toBinaryString(n));
+        System.out.println("Given No. in Octal is: "+Integer.toOctalString(n));
+        System.out.println("Given No. in Hexadecimal is: "+Integer.toHexString(n));
+    }
+}
--- 02.DataTypes-and-Variables/UseRadix.java
+++ 02.DataTypes-and-Variables/UseRadix.java
@@ -1,12 +1,36 @@
 import java.lang.*;
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+class Decimal_Number {
+    void CalculateDecimal() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter No. in Binary: ");
+        sc.useRadix(2);
+        // The no. given in radix is the base of that number system and it will auto convert into integer since there is nextInt(); Ex-1010 output - 10(in decimal system)
+        int binary_number = sc.nextInt();
+        System.out.println("Decimal No. is: "+binary_number);
+    }
+}
+class Octal_Number {
+    void CalculateOctal() {
+        System.out.print("Enter No. in Octal: ");
+        in.useRadix(8);
+        int octal_number = in.nextInt();
+        System.out.println("Octal No. is: "+octal_number);
+    }
+}
+// class Hex_Number {
+//     System.out.print("Enter No. is HexaDecimal: ");
+//         hx.useRadix(16);
+//         int hex_number = hx.nextInt();
+//         System.out.println("Hex No. is: "+hex_number);
+// }
 class UseRadix {
     public static void main(String[]args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a Number:- ");
-        sc.useRadix(2);
-        //The no. given in radix is the base of that number system and it will auto convert into integer since there is nextInt(); Ex-1010 output - 10(in decimal system)
-        int x = sc.nextInt();
-        System.out.println(x);
+        Decimal_Number obj = new Decimal_Number();
+        obj.CalculateDecimal();
+        Octal_Number obj1 = new Octal_Number();
+        obj1.CalculateOctal();
     }
 }
