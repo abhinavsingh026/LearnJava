90f9fb3d61e22e3be879c857130b25b181560ed2
abhinavsingh026 <abhinavkrsingh0258@gmail.com>
2025-06-09 18:35:32 +0530

    Added 3 types to find area

--- 05. Operators and Expressions/TriangleArea.java
+++ 05. Operators and Expressions/TriangleArea.java
@@ -8,10 +8,11 @@ class TriangleArea {
         base = sc.nextFloat();
         System.out.print("Enter Height of the Triangle: ");
         height = sc.nextFloat();
-        area =base*height/2;
+        area = base*height/2;
         float area1 = base*height*0.5f;
         float area2 = 1f/2f*base*height;
         System.out.println("Area of the Triangle is: "+area);
-        System.out.print("Area1 of the Triangle is: "+area1);
+        System.out.println("Area1 of the Triangle is: "+area1);
+        System.out.print("Area2 of the Triangle is: "+area2);
     }
 }
