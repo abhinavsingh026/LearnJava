3471e83a38105d99d70fb6897539cea3b1340aa3
abhinavsingh026 <abhinavkrsingh0258@gmail.com>
2025-06-09 18:11:58 +0530

    Triangle Area using Base and Height

---
+++ 05. Operators and Expressions/TriangleArea.java
@@ -0,0 +1,17 @@
+// Area of a Triangle using Base and Height //
+import java.util.*;
+class TriangleArea {
+    public static void main(String[]args) {
+        float base,height,area;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Base of the Triangle: ");
+        base = sc.nextFloat();
+        System.out.print("Enter Height of the Triangle: ");
+        height = sc.nextFloat();
+        area =base*height/2;
+        float area1 = base*height*0.5f;
+        float area2 = 1f/2f*base*height;
+        System.out.println("Area of the Triangle is: "+area);
+        System.out.print("Area1 of the Triangle is: "+area1);
+    }
+}
