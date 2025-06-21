db910f5c97a1fdd7fdde752a972c44bd0af6aa3d
abhinavsingh026 <abhinavkrsingh0258@gmail.com>
2025-06-21 18:25:06 +0530

    edited something

--- LabQuestions/Question6.java
+++ LabQuestions/Question6.java
@@ -3,11 +3,11 @@ class Question6 {
     public static void main(String[]args) {
         int count=0;
         Scanner sc = new Scanner(System.in);
-            System.out.print("Enter any Integer: ");
-            int n = sc.nextInt();
-            for(int i=1;i<=n;i++) {
-                if(n%i == 0)
-                count++;
+        System.out.print("Enter any Integer: ");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++) {
+            if(n%i == 0)
+            count++;
         }
         if(count == 2)
         System.out.print(n+" is a Prime Number");
