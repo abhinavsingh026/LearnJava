494d0fe91b7626724146c1e73c53452b1d508cb2
abhinavsingh026 <abhinavkrsingh0258@gmail.com>
2025-08-15 08:36:17 +0530

    added information of string object

--- 10.Methods/ObjectPassing.java
+++ 10.Methods/ObjectPassing.java
@@ -5,6 +5,10 @@ public class ObjectPassing {
     static void change2(int x,int value) {
         x = value;
     }
+    static String change3(String str) {
+        str = "Rakesh";
+        return str;
+    }
     public static void main(String []args) {
         int A[] = {2,3,4,5,6};
         change1(A,2,33);
@@ -13,6 +17,10 @@ public class ObjectPassing {
         System.out.println();
         int x = 10;
         change2(x,20);
-        System.out.print(x); //Primitive data type is unchanged in main method//
+        System.out.println(x); //Primitive data type is unchanged in main method//
+        String name = "Abhinav";
+        System.out.println(change3(name));
+        System.out.println(name); //String - Object is not changed in main method//
+                                  // Immutable //
     }
 }
